fde3644f45cb2f2bb1c1b7164d7397f597b7c614
ivanmasiero <alumno.599238@ies-azarquiel.es>
2025-04-27 11:42:34 +0200

    SQL en PLSQL terminado

---
+++ Paula/Paula-1dmlEnSql.sql
@@ -0,0 +1,61 @@
+SET SERVEROUTPUT ON
+--Solo podemos utilizar DML (insert, update, delete, drop y select) 
+--y TCL (Commit, Rollback y savepoints)
+--Utiliza acoplamiento temprano (tarda mas en compilar pero menos en ejecutar)
+--NO PODEMOS HACER
+
+--**********DML***************
+/*
+BEGIN
+     CREATE TABLE PEPE(
+     ID NUMEBER);
+END;
+*/
+
+--PERO SI:
+create table alumno (
+    id number,
+    nombre varchar2(30));
+
+
+--INSERT*********
+begin
+    insert into alumno (id,nombre)
+    values (1,'Ivan');
+    insert into alumno (id,nombre)
+    values (2,'Jose');
+    insert into alumno (id,nombre)
+    values (3,'Mario');
+    commit;
+end;
+/
+--UPDATE*********
+begin 
+    update alumno
+    set nombre = 'pepe'
+    where id =1;
+    commit;
+end;
+/
+--DELETE**********
+begin
+    delete from alumno
+    where id < 3;
+    commit;
+end;
+/
+--SELECT**********
+declare
+v_alumno alumno.nombre%type;
+begin
+    select nombre
+    into v_alumno
+    from alumno
+    where id = 3;
+    dbms_output.put_line(v_alumno);
+end;
+
+
+
+
+
