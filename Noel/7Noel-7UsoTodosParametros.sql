3f08654b72c70f8cc003ec866088034668a8852d
noel <alumno.705088@ies-azarquiel.es>
2025-04-28 08:30:26 +0200

    Update 7Noel-7UsoTodosParametros.sql

--- Noel/7Noel-7UsoTodosParametros.sql
+++ Noel/7Noel-7UsoTodosParametros.sql
@@ -3,14 +3,14 @@ DECLARE
     v_res number:=3;
     v_historial number;
 BEGIN
-    --Notación nominal
+    --Notación posicional
     sumaX(
     v_op,v_res,v_historial
     );
     dbms_output.put_line('El resultado es = ' || v_res || ', Último número sumado = ' || v_historial);
     v_op:=3; --le sumamos un numero distinto
     
-    --Notación posicional
+    --Notación nominal
     sumaX(
     p_res => v_res,
     p_historial => v_historial,
