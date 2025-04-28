2c0ac7e1d3dc140c85933dbd1bf020c29ce311fb
noel <alumno.705088@ies-azarquiel.es>
2025-04-28 08:50:43 +0200

    Update 7Noel-8Restricciones.sql

--- Noel/7Noel-8Restricciones.sql
+++ Noel/7Noel-8Restricciones.sql
@@ -1,9 +1,14 @@
+/*
+Este código concatena las un número con una letra.
+Con este programa podemos ver como funcionan las restricciones.
+*/
+
 create or replace function concatena(
     p_numero number,
     --p_letra varchar(1); --esto no funciona, ya que hereda las restricciones del parametro real
     p_letra char
     ) RETURN VARCHAR AS
-    v_aux varchar(9);
+    v_aux varchar(9); 
     BEGIN
         v_aux:= p_numero||p_letra;
         return v_aux;
@@ -12,7 +17,7 @@ create or replace function concatena(
 --Lo de arriba compilará, pero al utilizarlo no funciona, ya que concatena 9 y lo intenta guardar en 8.
 declare
 --al concatenar suma mas de 9
-v_texto varchar(9);
+v_texto varchar(8);
 v_numero number(8,0):=12345678;
 v_letra char(1):='L';
 BEGIN
