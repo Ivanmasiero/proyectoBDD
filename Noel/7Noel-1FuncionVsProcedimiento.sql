b475ae79a1c0227343e41c6de54510c122e72499
noel <alumno.705088@ies-azarquiel.es>
2025-04-28 08:37:58 +0200

    Update 7Noel-1FuncionVsProcedimiento.sql

--- Noel/7Noel-1FuncionVsProcedimiento.sql
+++ Noel/7Noel-1FuncionVsProcedimiento.sql
@@ -1,7 +1,15 @@
+/*
+En este codigo simplemente definimos un procedimiento y una función para sumar
+Aquí vamos a ver las diferencias entre procedimientos y funciones.
+
+Los procedimientos cuentan como una instrucción PL/SQL como tal,
+contrario a las funciones, que, como siempre, devuelven un valor,
+tienen que formar parte de otra instrucción.
+*/
 create or replace procedure pSuma (
     p_op1 in number,
     p_op2 in number,
-    p_res out number) is
+    p_res out number) IS
 BEGIN
     p_res:= p_op1+p_op2;
 END;
